d26e7c3c2d83ad6732543a9dad33da9ebf5df3f0
Luis Antonio <tyluis20@gmail.com>
2026-07-31 18:57:42 -0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,47 +8,44 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+        String path = System.getenv("PATH");
+        String[] pathDirs = path.split(":");
+
         while (true) {
             System.out.print("$ ");
             String command = input.nextLine();
-            if(command.equals("exit")){
+            if (command.equals("exit")) {
                 break;
             }
-            else if(command.startsWith("type")) {
+            else if (command.startsWith("type")) {
                 String cmdSubtype = command.substring(5);
+                String output = "";
+
                 List<String> commands = Arrays.asList("type", "echo", "exit", "grep");
                 boolean existComand = commands.stream().anyMatch(cmd -> cmd.equals(cmdSubtype));
-                boolean existtype = false;
-                String path = System.getenv("PATH");
-                String[] pathDirs = path.split(":");
-//                for(String dir : pathDirs){
-//                    File dirFile = new File(dir, existComand);
-//                    if(dirFile.isDirectory()){
-//                        System.out.println( cmdSubtype + " is " + dirFile.getAbsolutePath());
-//                    }
-                for(int i = 0; i < pathDirs.length; i++){
-                 File file = new File(pathDirs[i], cmdSubtype);
-                 if(file.exists() && file.canExecute()){
-                     System.out.println(cmdSubtype + " is " + file.getAbsolutePath());
-                 }
-                }
-                if(existComand){
-                    System.out.println(cmdSubtype + " is a shell builtin");
-                }else {
-                    System.out.println( cmdSubtype+ ": not found");
-                }
 
-            }
-            else if(command.startsWith("echo ")){
+                if (!existComand) {
+                    for (int i = 0; i < pathDirs.length; i++) {
+                        File file = new File(pathDirs[i], cmdSubtype);
+                        if (file.exists() && file.canExecute()) {
+                            output = cmdSubtype + " is " + file.getAbsolutePath();
+                        }
+                    }
+                    if (output.isEmpty()) {
+                        output = cmdSubtype + ": not found";
+                    }
+                } else {
+                    output = cmdSubtype + " is a shell builtin";
+                }
+                System.out.println(output);
+            } else if (command.startsWith("echo ")) {
                 System.out.println(command.substring(5));
-            }
-            else if(command.startsWith("grep ")){
-               // System.out.println(command.substring(5));
-
-            }
-            else {
+            } else if (command.startsWith("grep ")) {
+                // System.out.println(command.substring(5));
+            } else {
                 System.out.println(command + ": command not found");
             }
         }
     }
+
 }
---
+++ src/main/java/Main2.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String path = System.getenv("PATH");
+        String[] pathDirs = path.split(":");
+
+        while (true) {
+            System.out.print("$ ");
+            String command = sc.nextLine();
+
+            if (command.equals("exit")) {
+                break;
+            } else if (command.startsWith("echo")) {
+                System.out.println(command.substring(5));
+            } else if (command.startsWith("type")) {
+                String typeArg = command.substring(5);
+                System.out.println(type(typeArg));
+            } else {
+                System.out.println(command + ": command not found");
+            }
+        }
+
+        sc.close();
+    }
+
+    public static String type(String command) {
+        String[] commands = {"exit", "echo", "type"};
+        String path = System.getenv("PATH");
+        String[] pathDirs = path.split(":");
+
+        boolean isBuiltIn = false;
+        for (int i = 0; i < commands.length; i++) {
+            if (commands[i].equals(command)) {
+                return command + " is a shell builtin";
+            }
+        }
+
+        for (int i = 0; i < pathDirs.length; i++) {
+            File file = new File(pathDirs[i], command);
+            if (file.exists() && file.canExecute()) {
+                return command + " is " + file.getAbsolutePath();
+            }
+        }
+
+        return command + ": not found";
+    }
+}
